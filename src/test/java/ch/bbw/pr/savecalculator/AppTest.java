1dc2faeedb6e9a769b2d8dd1cb2650e6281c592c
Marijaradovanoviic <Marija.Radovanovic@swisscom.com>
2022-01-20 16:53:41 +0100

    before und after und Namensgebung der Testfunktion
    Hier sind die ersten beiden Tests

--- src/test/java/ch/bbw/pr/savecalculator/AppTest.java
+++ src/test/java/ch/bbw/pr/savecalculator/AppTest.java
@@ -1,7 +1,9 @@
 package ch.bbw.pr.savecalculator;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,9 +14,28 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    private SaveCalculator teste;
+@Before
+public void createSavCalculatorTeste() {
+    teste = new SaveCalculator();
+}
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
+    @Test
+public void testZweiPositiveIsOk() {
+    int value1 = 10;
+    int value2 = 20;
+    assertEquals(30, teste.summe(value1, value2));
+}
+
+@Test
+public void testZweiNegativeIstOk() {
+    SaveCalculator teste = new SaveCalculator();
+    int value1 = 10;
+    int value2 = 20;
+    assertEquals(-10, teste.subtraktion(value1, value2));
+}
 }
